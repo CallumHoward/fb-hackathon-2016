acb1a0d1007f4a405d6160549263a4a5627ccb91
Albert Kurniawan <kurniawan.albert19@gmail.com>
2016-05-01 01:38:03 +1000

    Working version

--- app/imports/client/App.jsx
+++ app/imports/client/App.jsx
@@ -5,6 +5,11 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { streamToken: '', viewUrl: '' };
+  }
+
   getTasks() {
     return [
       { _id: 1, text: 'Stream token' },
@@ -19,13 +24,31 @@ export default class App extends Component {
   }
 
   onButtonClick() {
+    const accessToken = Secrets.findOne({}).accessToken;
     HTTP.post(
-      'https://graph.facebook.com/v2.6/590615017770118/live_videos', 
+      'https://graph.facebook.com/v2.6/856086361161946/live_videos', 
       { params: 
-        { access_token: '' }
+        { access_token: accessToken }
       },
-      function(err, res) {
+      (err, res) => {
         console.log(res);
+        if (res.data) {
+          const streamUrl = res.data.secure_stream_url;
+          const streamToken = streamUrl.substring(streamUrl.lastIndexOf('/')+1);
+          const streamId = res.data.id;
+          HTTP.get(
+            'https://graph.facebook.com/v2.6/'+streamId,
+            { params: { fields: 'permalink_url', access_token: accessToken } },
+            (err, res2) => {
+              if (res2.data) {
+                const viewUrl = 'https://www.facebook.com' + res2.data.permalink_url;
+                this.setState({ viewUrl });
+                console.log(res2);
+              }
+            }
+          );
+          this.setState({ streamToken });
+        }
       }
     );
   }
@@ -38,8 +61,20 @@ export default class App extends Component {
         </header>
 
         <ul>
-          {this.renderTasks()}
+          <li>
+            <span style={{width: '140px', display: 'inline-block', textAlign: 'right'}}>Stream Token:</span>
+            <input className="text" type="text"
+              value={this.state.streamToken} readonly="true"
+            />
+          </li>
+          <li>
+            <span style={{width: '140px', display: 'inline-block', textAlign: 'right'}}>URL:</span>
+            <input className="text" type="text" 
+              value={this.state.viewUrl} readonly="true"
+            />
+          </li>
         </ul>
+
         <button onClick={this.onButtonClick.bind(this)}>Button</button>
       </div>
     );
--- app/server/main.js
+++ app/server/main.js
@@ -3,7 +3,10 @@ import { Meteor } from 'meteor/meteor';
 import serverIndex from '../imports/server/index.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  const hasSecrets = Secrets.findOne({});
+  if (!hasSecrets) {
+    Secrets.insert({ accessToken: process.env.ACCESS_TOKEN });
+  }
 });
 
 Picker.route('/', serverIndex.indexRouteHandler);
