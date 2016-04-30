b01e2b2399f87f38ddfb2d05389a8ee3f1b19eb3
erickchaosz <erickwu8@hotmail.com>
2016-04-30 18:12:32 +1000

    Add ui

--- app/imports/client/App.jsx
+++ app/imports/client/App.jsx
@@ -6,9 +6,8 @@ import Task from './Task.jsx';
 export default class App extends Component {
   getTasks() {
     return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
+      { _id: 1, text: 'Stream token' },
+      { _id: 2, text: 'URL' },
     ];
   }
 
@@ -22,7 +21,7 @@ export default class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Streamline</h1>
         </header>
 
         <ul>
