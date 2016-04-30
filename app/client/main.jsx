a8d16993806db5110cca266322fdadf3f02c8737
Albert Kurniawan <kurniawan.albert19@gmail.com>
2016-04-30 18:41:39 +1000

    Changed font to Noto Sans

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -11,3 +11,18 @@ FlowRouter.route('/fbapp', {
     mount(App);
   }
 });
+
+Meteor.startup(() => {
+  // Font Noto Sans
+   WebFontConfig = {
+    google: { families: [ 'Noto+Sans::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+});
